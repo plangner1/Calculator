6d4cd8e230717a14f015648c066130ad695f6110
plangner1 <mpiotr.langner@gmail.com>
2019-08-07 19:24:31 +0200

    Add new operations: multiplication and division

--- src/main/java/com/example/learning/calculator/CalculatorImpl.java
+++ src/main/java/com/example/learning/calculator/CalculatorImpl.java
@@ -10,4 +10,12 @@ public class CalculatorImpl implements Calculator {
     public int subtraction(int a, int b) {
         return a-b;
     }
+
+    public int multiplication(int a, int b) {
+        return a*b;
+    }
+
+    public int division(int a, int b) {
+        return a/b;
+    }
 }
